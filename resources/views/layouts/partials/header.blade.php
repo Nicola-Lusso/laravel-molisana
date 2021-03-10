d75111d9aa0b673de174f419f3437bd1dff1d5e4
Nicola <nicolusso@gmail.com>
2021-03-10 17:17:57 +0100

    Schema iniziale header

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <div id="logo">
+        <img src="" alt="">
+    </div>
+    <div class="main-menu">
+        <nav>
+            <ul>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
