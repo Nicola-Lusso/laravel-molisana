3243c29daf711e52ee3c1df8c100fc60df8c2c5d
Nicola <nicolusso@gmail.com>
2021-03-11 17:11:46 +0100

    collegamento prodotti (route)

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,10 +5,13 @@
 @section('content')
 
 <div class="box-container">
-    @foreach ($formati as $item)
+    @foreach ($formati as $key => $formato)
         <div class="box">
-            <img src="{{ $item['src'] }}" alt="">
-            <p class="descrizione">{{ $item['titolo'] }} - {{ $item['tipo'] }}</p>
+            <img src="{{ $formato['src'] }}" alt="">
+            <div class="overlay">
+                <a href=" {{ route('pagina-dettagli', ['id' => $key]) }} "> {{$formato['titolo']}} </a>
+            </div>
+            {{-- <p class="descrizione">{{ $formato['titolo'] }} - {{ $formato['tipo'] }}</p> --}}
         </div>
     @endforeach
 </div>
